d5a7e69d1158e819a74b0eb64937a12c4316e99a
NoobMahbub <Mahbubbengali@gmail.com>
2024-11-28 23:59:49 +0600

    removed all exe files

---
+++ gdc.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    while(a!=0){
+        
+        a = b/a
+        a++;
+    }    
+}
+
+
+/*
+// Euclidean algorithm for GCD
+a  b  remainder
+30 45  12
+    `  6
+12 30
+6  12  0
+0  6
+
+do until a != 0
+
+*/
+
