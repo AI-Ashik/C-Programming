4aa7c916e55924aebd3962e181f00541eed46211
AI-Ashik <arifulislamashik07@gmail.com>
2024-12-04 23:50:53 +0600

    added basic idea of dataTypes, added useful notes

---
+++ Data Types/dataTypes.c
@@ -0,0 +1,20 @@
+// This is for the basic Idea of DataTypes in C Programming
+
+// Create variables
+int myNum = 5;             // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'd';       // Character
+
+// Print variables
+printf("%d\n", myNum); // %d or %i
+printf("%f\n", myFloatNum); // %f
+printf("%c\n", myLetter); // %c
+
+
+// Some Useful notes about data types
+/*
+1. In char data types - we have to use single quote '' and one character at a time, if we use full words it will print the last digit of the word.
+If we want to use string we have to use [] after variable name, specify the datatype of string using %s and use double quote "".
+*/
+
+
